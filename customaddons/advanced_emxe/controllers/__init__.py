4611aa78eaca51c156d7be32549f940b11e1ffbc
sonpk <sonpk>
2025-03-23 22:27:47 +0700

    main

--- customaddons/advanced_emxe/controllers/__init__.py
+++ customaddons/advanced_emxe/controllers/__init__.py
@@ -4,4 +4,4 @@ from . import home
 from . import driver
 from . import session
 from . import controller
-from . import emxe_notification_controller
+# from . import emxe_notification_controller
--- customaddons/advanced_emxe/controllers/controller.py
+++ customaddons/advanced_emxe/controllers/controller.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 import logging
 import math
 import requests
+import re
 
 
 def haversine(lat1, lon1, lat2, lon2):
@@ -17,6 +18,26 @@ def haversine(lat1, lon1, lat2, lon2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     d = R * c
     return round(d, 2)
+
+def _create_flutter_log(env, name=None, type=None, description=None):
+    """
+    Logging for api
+    param
+    type: [request, response, error].
+    """
+    env['emxe.flutter.log'].sudo().create({
+        'name': name,
+        'type': type,
+        'description': description,
+    })
+
+
+def strip_html_tags(html):
+    clean = re.compile('<.*?>')
+    text = re.sub(clean, '', html)
+    text.replace('\n', '')
+    text = re.sub(r'\s{2,}', ' ', text).strip()
+    return text
 
 # function to calculate distance by real road map with google api
 def get_distance(lat1, lon1, lat2, lon2):
@@ -1707,3 +1728,83 @@ class EMXEFlutterApi(http.Controller):
                 'code': 400,
                 'message': e,
             }
+
+    @http.route('/emxe_api/add_registration_id', auth='user', csrf=False, type='json')
+    def emxe_add_registration_id(self, **kw):
+        try:
+            _create_flutter_log(request.env, name='add_registration_id', description=str(kw), type='request')
+            user = request.env.user
+            if kw.get('registration_id'):
+                reg = request.env['emxe.mobile.registration.token'].sudo().search(
+                    [('token', '=', kw.get('registration_id'))], limit=1)
+                if not reg:
+                    request.env['emxe.mobile.registration.token'].sudo().create(
+                        {
+                            'user_id': user.id,
+                            'token': kw.get('registration_id'),
+                        }
+                    )
+                else:
+                    reg.sudo().write({'user_id': user.id})
+                return {
+                    "status": "success",
+                    "code": 200,
+                    "message": "Đăng ký thành công",
+                    "data": {
+                        "success": True
+                    }
+                }
+        except Exception as e:
+            _create_flutter_log(request.env, name='add_registration_id', description=str(e), type='error')
+            return {
+                "status": "fail",
+                "code": 400,
+                "message": e,
+                "data": {
+                    "success": False
+                }
+            }
+
+    @http.route('/emxe_api/get_notification_message', auth='user', csrf=False, type='json')
+    def emxe_get_notification_message(self, **kw):
+        try:
+            _create_flutter_log(request.env, name='get_notification_message', description=str(kw), type='request')
+            user = request.env.user
+            message_list = []
+            offset = kw.get('index') if kw.get('index') else 0
+            limit = kw.get('offset') if kw.get('offset') else 80
+            if user.partner_id:
+                message = request.env['mail.message'].search(
+                    [('partner_ids', 'in', user.partner_id.id), ('message_type', '=', 'notification'),
+                     ('model', 'in', ['hc.trip'])], offset=offset, limit=limit, order='id desc')
+                print(message)
+                if len(message) > 0:
+                    for mess in message:
+                        val = {
+                            'create_on': mess.create_date,
+                            'message_id': mess.id,
+                            'type': 'payment' if 'của bạn đã được hoàn thành' in mess.body else 'trip',
+                            'message_name': 'Thông báo',
+                            'message_body': strip_html_tags(mess.body) if mess.body else None,
+                            'res_id': mess.res_id,
+                        }
+                        message_list.append(val)
+            return {
+                "status": "success",
+                "code": 200,
+                "message": "success",
+                "data": message_list
+            }
+
+
+        except Exception as e:
+            _create_flutter_log(request.env, name='get_notification_message', description=str(e), type='error')
+            return {
+                "status": "fail",
+                "code": 400,
+                "message": e,
+                "data": {
+                    "success": False
+                }
+            }
+
